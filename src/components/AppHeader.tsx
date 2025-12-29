911b0e5e536eb7fac4d22356aedbbd543cb964fc
Nitin Chandran Nair <nitin.dreamern89@gmail.com>
2025-12-29 16:33:19 +0530

    chore: Minor style changes

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -5,7 +5,7 @@ import { ThemeToggle } from "@/components/ThemeToggle";
 export function AppHeader() {
 	return (
 		<header className="header">
-			<h1 className="display-lg font-semibold bg-surface-primary">
+			<h1 className="display-lg font-semibold">
 				Frontend Foundation Template Starter
 			</h1>
 			<ThemeToggle />
